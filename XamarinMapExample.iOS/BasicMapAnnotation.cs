dc1d4dc8ae5a1372aec0aae37a3e0f4f7434ac8a
raja95shah <raja95shah@gmail.com>
2016-08-06 04:03:08 +0800

    initial commit

---
+++ XamarinMapExample.iOS/BasicMapAnnotation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using CoreLocation;
+using MapKit;
+
+namespace TrabbleCrossPlatformExperiment.iOS
+{
+    class BasicMapAnnotation : MKAnnotation
+    {
+
+        CLLocationCoordinate2D coord;
+        string title, subtitle;
+
+        public override CLLocationCoordinate2D Coordinate { get { return coord; } }
+        public override void SetCoordinate(CLLocationCoordinate2D value)
+        {
+            coord = value;
+        }
+        public override string Title { get { return title; } }
+        public override string Subtitle { get { return subtitle; } }
+        public BasicMapAnnotation(CLLocationCoordinate2D coordinate, string title, string subtitle)
+        {
+            this.coord = coordinate;
+            this.title = title;
+            this.subtitle = subtitle;
+        }
+
+        public double KilometresToLatitudeDegrees(double kms)
+        {
+            double earthRadius = 6371.0; // in kms
+            double radiansToDegrees = 180.0 / Math.PI;
+            return (kms / earthRadius) * radiansToDegrees;
+        }
+
+        public double KilometresToLongitudeDegrees(double kms, double atLatitude)
+        {
+            double earthRadius = 6371.0; // in kms
+            double degreesToRadians = Math.PI / 180.0;
+            double radiansToDegrees = 180.0 / Math.PI;
+            // derive the earth's radius at that point in latitude
+            double radiusAtLatitude = earthRadius * Math.Cos(atLatitude * degreesToRadians);
+            return (kms / radiusAtLatitude) * radiansToDegrees;
+        }
+    }
+}
+
---
+++ XamarinMapExample.iOS/ViewController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using CoreLocation;
+using MapKit;
+using TrabbleCrossPlatformExperiment.iOS;
+using UIKit;
+
+namespace XamarinMapExample.iOS
+{
+    public partial class ViewController : UIViewController
+    {
+
+        MKMapView mapView;
+        private CLLocationCoordinate2D coords;
+        private const int radius = 5;
+        private CLLocationManager locationManager;
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            setUpView();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        void setUpView()
+        {
+            mapView = new MKMapView(View.Bounds);
+            mapView.AutoresizingMask = UIViewAutoresizing.FlexibleDimensions;
+            View.AddSubview(mapView);
+
+            locationManager = new CLLocationManager();
+            locationManager.RequestWhenInUseAuthorization();
+            locationManager.StartUpdatingLocation();
+
+            mapView.UserTrackingMode = MKUserTrackingMode.None;
+            mapView.ShowsUserLocation = true;
+              
+            coords = new CLLocationCoordinate2D(locationManager.Location.Coordinate.Latitude,locationManager.Location.Coordinate.Longitude);
+            var annotation = new BasicMapAnnotation(coords, "Current Location", null);
+            mapView.AddAnnotation(annotation);
+
+
+            var span = new MKCoordinateSpan(annotation.KilometresToLatitudeDegrees(radius), annotation.KilometresToLongitudeDegrees(radius, coords.Latitude));
+            mapView.Region = new MKCoordinateRegion(coords, span);
+
+        }
+    }
+}
+
